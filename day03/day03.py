dafc983e08591b384c9b64109b6d1b03443f18a7
William Harwood <harwood.william@gmail.com>
2025-12-06 10:01:35 -0800

    completed day 3

---
+++ day03/day03.py
@@ -0,0 +1,42 @@
+def do_part_1(input_lines):
+    banks: list[BatteryBank] = []
+    for line in input_lines:
+        bank = BatteryBank()
+        bank.parse_cell_str(line)
+        banks.append(bank)
+    result = 0
+    for bank in banks:
+        result += bank.get_max_joltage(2)
+    print(f"Max joltage: {result}")
+
+def do_part_2(input_lines):
+    banks: list[BatteryBank] = []
+    for line in input_lines:
+        bank = BatteryBank()
+        bank.parse_cell_str(line)
+        banks.append(bank)
+    result = 0
+    for bank in banks:
+        result += bank.get_max_joltage(12)
+    print(f"Max joltage: {result}")
+
+class BatteryBank:
+    def __init__(self, cells: list[int] = []):
+        self.cells = cells
+
+    def parse_cell_str(self, cell_str: str):
+        self.cells = [int(c) for c in cell_str]
+
+    def get_max_joltage(self, cell_qty: int) -> int:
+        stack = []
+        drop = len(self.cells) - cell_qty  # how many digits we are allowed to drop
+        
+        for n in self.cells:
+            while drop and stack and stack[-1] < n:
+                stack.pop()
+                drop -= 1
+            stack.append(n)
+        
+        # Only take k digits (stack may be longer)
+        result = stack[:cell_qty]
+        return int("".join(map(str, result)))
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from day01 import day01
+from day02 import day02
+from day03 import day03
 from helpers import helpers
 
-input_lines = helpers.parse_input("day01/input.txt")
-day01.do_part_1(input_lines)
-day01.do_part_2(input_lines)
+# input_lines = helpers.parse_input("day01/input.txt")
+# day01.do_part_1(input_lines)
+# day01.do_part_2(input_lines)
 # input_lines = helpers.parse_input("day02/input.txt")
 # day02.do_part_1(input_lines)
 # day02.do_part_2(input_lines)
-# input_lines = helpers.parse_input("day03/input.txt")
-# day03.do_part_1(input_lines)
+input_lines = helpers.parse_input("day03/input.txt")
+day03.do_part_1(input_lines)
+day03.do_part_2(input_lines)
 # input_lines = helpers.parse_input("day03/input.txt")
 # day03.do_part_1(input_lines)
 # input_lines = helpers.parse_input("day04/input.txt")
