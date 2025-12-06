69d53319c38c09c3619515a59d5c294373398028
William Harwood <harwood.william@gmail.com>
2025-12-06 13:53:21 -0800

    feat: extended grid and finished day4

---
+++ day04/day04.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+import grid
+
+def do_part_1(input_lines):
+    grid = make_factory(input_lines)
+    accessible_count = 0
+    for node in grid.nodes:
+        if not node.is_paper():
+            continue
+        if grid.is_accessible(node):
+            accessible_count += 1
+    print(f"Forklift accessible rolls: {accessible_count}")
+
+def do_part_2(input_lines):
+    grid = make_factory(input_lines)
+    accessible_count = 0
+    iteration = 0
+    while True:
+        print(f"Starting iteration {iteration}")
+        iteration += 1
+        accessible_nodes = []
+        for node in grid.nodes:
+            if not node.is_paper():
+                continue
+            if grid.is_accessible(node):
+                accessible_nodes.append(node)
+        if not accessible_nodes:
+            break
+        accessible_count += len(accessible_nodes)
+        grid.remove_paper(accessible_nodes)
+    print(f"Removed rolls: {accessible_count}")
+
+def make_factory(input_lines) -> paper_factory_grid:
+    nodes: list[paper_factory_node] = []
+    for y, line in enumerate(input_lines):
+        for x, char in enumerate(line):
+            nodes.append(paper_factory_node(x, y, char))
+    return paper_factory_grid(nodes)
+
+class paper_factory_node(grid.grid_node):
+    def __init__(self, x: int, y: int, contents: str) -> None:
+        super().__init__(x, y)
+        if contents not in ['.', '@']:
+            raise ValueError(f"Invalid contents for paper factory node: {contents}")
+        self.contents = contents
+
+    def is_paper(self) -> bool:
+        return self.contents == '@'
+    
+class paper_factory_grid(grid.grid[paper_factory_node]):
+    def __init__(self, nodes: list[paper_factory_node]) -> None:
+        super().__init__(nodes)
+
+    def is_accessible(self, node: paper_factory_node, surround_qty_limit: int = 4) -> bool:
+        if not node.is_paper():
+            raise ValueError("Node is not paper.")
+        neighbors = self.get_neighbors(node, diagonals=True)
+        if len([n for n in neighbors if n.is_paper()]) < surround_qty_limit:
+            return True
+        return False
+    
+    def remove_paper(self, nodes: list[paper_factory_node]) -> None:
+        for node in nodes:
+            if not node.is_paper():
+                raise ValueError("Node is not paper.")
+            node.contents = '.'
---
+++ grid.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+from typing import Iterable, TypeVar, Optional, Generic
+
+T = TypeVar("T", bound="grid_node")
+
+class grid_node():
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+class grid(Generic[T]):
+    def __init__(self, nodes: Optional[Iterable[T]] = None, limits: Optional[tuple[int, int, int, int]] = None) -> None:
+        self.nodes: list[T] = list(nodes) if nodes is not None else []
+        self.bounds = None
+        if limits is not None:
+            self.bounds = grid.Bounds(limits[0], limits[1], limits[2], limits[3])
+    
+    class Bounds:
+        def __init__(self, x_min: int, x_max: int, y_min: int, y_max: int) -> None:
+            self.x_min = x_min
+            self.x_max = x_max
+            self.y_min = y_min
+            self.y_max = y_max
+
+        def __str__(self) -> str:
+            return f"({self.x_min}, {self.x_max}, {self.y_min}, {self.y_max})"
+
+    def get_len_x(self) -> int:
+        return max([node.x for node in self.nodes]) - min([node.x for node in self.nodes]) + 1
+    
+    def get_len_y(self) -> int:
+        return max([node.y for node in self.nodes]) - min([node.y for node in self.nodes]) + 1
+    
+    def get_max_x(self) -> int:
+        return max([node.x for node in self.nodes if self.nodes is not None])
+    
+    def get_max_y(self) -> int:
+        return max([node.y for node in self.nodes if self.nodes is not None])
+    
+    def get_min_x(self) -> int:
+        return min([node.x for node in self.nodes if self.nodes is not None])
+    
+    def get_min_y(self) -> int:
+        return min([node.y for node in self.nodes if self.nodes is not None])
+    
+    def is_in_grid(self, x: int, y: int) -> bool:
+        return any(node.x == x and node.y == y for node in self.nodes)
+    
+    def get_node(self, x: int, y: int) -> Optional[T]:
+        for node in self.nodes:
+            if node.x == x and node.y == y:
+                return node
+        raise ValueError(f"No node at ({x},{y}).")
+
+    def get_neighbors(self, node: T, diagonals: bool = False) -> list[T]:
+        neighbors = []
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        for dx, dy in directions:
+            x, y = node.x + dx, node.y + dy
+            if self.is_in_grid(x, y):
+                neighbors.append(self.get_node(x, y))
+        if diagonals:
+            diagonal_directions = [(1,1), (1,-1), (-1,1), (-1,-1)]
+            for dx, dy in diagonal_directions:
+                x, y = node.x + dx, node.y + dy
+                if self.is_in_grid(x, y):
+                    neighbors.append(self.get_node(x, y))
+        return neighbors
+
+    def get_node_up(self, node: T) -> Optional[T] | None:
+        neighbors = self.get_neighbors(node)
+        for n in [n for n in neighbors if n is not None]:
+            if n.x == node.x and n.y+1 == node.y:
+                return n
+        return None
+    
+    def get_node_down(self, node: T) -> Optional[T] | None:
+        neighbors = self.get_neighbors(node)
+        for n in [n for n in neighbors if n is not None]:
+            if n.x == node.x and n.y-1 == node.y:
+                return n
+        return None
+    
+    def get_node_right(self, node: T) -> Optional[T] | None:
+        neighbors = self.get_neighbors(node)
+        for n in [n for n in neighbors if n is not None]:
+            if n.x-1 == node.x and n.y == node.y:
+                return n
+        return None
+    
+    def get_node_left(self, node: T) -> Optional[T] | None:
+        neighbors = self.get_neighbors(node)
+        for n in [n for n in neighbors if n is not None]:
+            if n.x+1 == node.x and n.y == node.y:
+                return n
+        return None
