0f48d8cf72960574023625152dadf6ff0452a589
William Harwood <will@jellywood.org>
2025-12-23 21:57:12 -0800

    fix: added example txt files

--- tests/test_day05.py
+++ tests/test_day05.py
@@ -3,11 +3,11 @@ from src.day05 import do_part_1, do_part_2
 from src.helpers import helpers
 
 def test_day05_part_1_example():
-    file = Path("./input/day05_example.txt")
+    file = Path("./tests/day05_example.txt")
     input = helpers.parse_input(file)
     assert do_part_1(input) == 3
 
 def test_day05_part_2_example():
-    file = Path("./input/day05_example.txt")
+    file = Path("./tests/day05_example.txt")
     input = helpers.parse_input(file)
     assert do_part_2(input) == 14
